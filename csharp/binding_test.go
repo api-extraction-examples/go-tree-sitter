90efb9e9e243bd178a3c97f4e666d12dccc156e2
Daniel Martins <daniel@nightvision.net>
2025-01-06 16:23:57 +0200

    Add binding test that has no error anymore

--- csharp/binding_test.go
+++ csharp/binding_test.go
@@ -2,6 +2,7 @@ package csharp_test
 
 import (
 	"context"
+	"os"
 	"testing"
 
 	sitter "github.com/api-extraction-examples/go-tree-sitter"
@@ -16,6 +17,21 @@ func TestGrammar(t *testing.T) {
 	assert.NoError(err)
 	assert.Equal(
 		"(compilation_unit (using_directive (qualified_name qualifier: (identifier) name: (identifier))))",
+		n.String(),
+	)
+}
+
+func TestGrammar2(t *testing.T) {
+	assert := assert.New(t)
+
+	content, err := os.ReadFile("testt.cs") // testt.cs is: https://github.com/Universalis-FFXIV/Universalis/blob/bc38866d8cbc85ed95df46f432bc896b697f218c/src/Universalis.Application/Controllers/V2/WebSocketController.cs#L10
+	assert.NoError(err)
+
+	n, err := sitter.ParseCtx(context.Background(), content, csharp.GetLanguage())
+	assert.NoError(err)
+	expected := "(compilation_unit (using_directive (qualified_name qualifier: (qualified_name qualifier: (identifier) name: (identifier)) name: (identifier))) (using_directive (qualified_name qualifier: (qualified_name qualifier: (identifier) name: (identifier)) name: (identifier))) (using_directive (qualified_name qualifier: (identifier) name: (identifier))) (using_directive (qualified_name qualifier: (qualified_name qualifier: (identifier) name: (identifier)) name: (identifier))) (using_directive (qualified_name qualifier: (qualified_name qualifier: (identifier) name: (identifier)) name: (identifier))) (using_directive (qualified_name qualifier: (qualified_name qualifier: (identifier) name: (identifier)) name: (identifier))) (file_scoped_namespace_declaration name: (qualified_name qualifier: (qualified_name qualifier: (qualified_name qualifier: (identifier) name: (identifier)) name: (identifier)) name: (identifier))) (class_declaration (attribute_list (attribute name: (identifier))) (attribute_list (attribute name: (identifier) (attribute_argument_list (attribute_argument (string_literal (string_literal_content)))))) (attribute_list (attribute name: (identifier) (attribute_argument_list (attribute_argument (string_literal (string_literal_content)))))) (attribute_list (attribute name: (identifier) (attribute_argument_list (attribute_argument (string_literal (string_literal_content)))))) (preproc_if_in_attribute_list condition: (unary_expression argument: (identifier)) (attribute_list (attribute name: (identifier) (attribute_argument_list (attribute_argument (assignment_expression left: (identifier) right: (boolean_literal))))))) (modifier) name: (identifier) (base_list (identifier)) body: (declaration_list (field_declaration (modifier) (modifier) (variable_declaration type: (identifier) (variable_declarator name: (identifier)))) (constructor_declaration (modifier) name: (identifier) parameters: (parameter_list (parameter type: (identifier) name: (identifier))) body: (block (expression_statement (assignment_expression left: (identifier) right: (identifier))))) (comment) (comment) (comment) (method_declaration (attribute_list (attribute name: (identifier))) (attribute_list (attribute name: (identifier) (attribute_argument_list (attribute_argument (string_literal (string_literal_content)))))) (attribute_list (attribute name: (identifier) (attribute_argument_list (attribute_argument (string_literal (string_literal_content)))))) (attribute_list (attribute name: (identifier) (attribute_argument_list (attribute_argument (string_literal (string_literal_content)))))) (modifier) returns: (identifier) name: (identifier) parameters: (parameter_list (parameter type: (identifier) name: (identifier) (default_expression))) body: (block (return_statement (invocation_expression function: (identifier) arguments: (argument_list (argument (identifier))))))) (comment) (comment) (comment) (method_declaration (attribute_list (attribute name: (identifier))) (attribute_list (attribute name: (identifier) (attribute_argument_list (attribute_argument (string_literal (string_literal_content)))))) (attribute_list (attribute name: (identifier) (attribute_argument_list (attribute_argument (string_literal (string_literal_content)))))) (attribute_list (attribute name: (identifier) (attribute_argument_list (attribute_argument (string_literal (string_literal_content)))))) (modifier) (modifier) returns: (identifier) name: (identifier) parameters: (parameter_list (parameter type: (identifier) name: (identifier) (default_expression))) body: (block (if_statement condition: (member_access_expression expression: (member_access_expression expression: (identifier) name: (identifier)) name: (identifier)) consequence: (block (expression_statement (await_expression (invocation_expression function: (member_access_expression expression: (identifier) name: (identifier)) arguments: (argument_list (argument (identifier)) (argument (identifier)) (argument (identifier))))))) alternative: (block (expression_statement (assignment_expression left: (member_access_expression expression: (member_access_expression expression: (identifier) name: (identifier)) name: (identifier)) right: (member_access_expression expression: (identifier) name: (identifier)))))))))))"
+	assert.Equal(
+		expected,
 		n.String(),
 	)
 }
---
+++ csharp/testt.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+using Universalis.Application.Realtime;
+using Universalis.Application.Swagger;
+
+namespace Universalis.Application.Controllers.V2;
+
+[ApiController]
+[ApiVersion("1")]
+[ApiVersion("2")]
+[Route("api")]
+#if !DEBUG
+[ApiExplorerSettings(IgnoreApi = true)]
+#endif
+public class WebSocketController : ControllerBase
+{
+    private readonly ISocketProcessor _socketProcessor;
+
+    public WebSocketController(ISocketProcessor socketProcessor)
+    {
+        _socketProcessor = socketProcessor;
+    }
+
+    /// <summary>
+    /// Connect to the WebSocket endpoint of the API. Requires a valid WebSocket client.
+    /// </summary>
+    [HttpGet]
+    [MapToApiVersion("1")]
+    [Route("ws")]
+    [ApiTag("WebSocket")]
+    public Task Get(CancellationToken cancellationToken = default)
+    {
+        return GetV2(cancellationToken);
+    }
+
+    /// <summary>
+    /// Connect to the WebSocket endpoint of the API. Requires a valid WebSocket client.
+    /// </summary>
+    [HttpGet]
+    [MapToApiVersion("2")]
+    [Route("v{version:apiVersion}/ws")]
+    [ApiTag("WebSocket")]
+    public async Task GetV2(CancellationToken cancellationToken = default)
+    {
+        if (HttpContext.WebSockets.IsWebSocketRequest)
+        {
+            await WebSocketHandler.Connect(HttpContext, _socketProcessor, cancellationToken);
+        }
+        else
+        {
+            HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+        }
+    }
+}
